b1d91940fd690eaa5ef9ae1906bb2ec774d858e1
Thas-Tayapongsak <hwunhwun@gmail.com>
2025-03-13 11:54:25 +0700

    fixed onnx export

--- __main__.py
+++ __main__.py
@@ -1,7 +1,7 @@
 from exporters import export_onnx, parse_arguments
 
 def main(args):
-    export_onnx(args.repo_id, args.task, args.output)
+    export_onnx(args.repo_id, args.task, args.output_path, args.abs_path)
 
 if __name__ == '__main__':
     args = parse_arguments()
--- exporters/main.py
+++ exporters/main.py
@@ -1,5 +1,6 @@
-from transformers import AutoConfig, AutoModel, AutoModelForImageClassification
-from optimum.exporters.onnx import export
+from transformers import AutoConfig, AutoModel, AutoModelForImageClassification, PretrainedConfig
+from optimum.exporters.onnx import export, OnnxConfig
+from optimum.exporters import TasksManager
 from pathlib import Path
 
 import argparse
@@ -19,25 +20,40 @@ def create_model(repo_id: str, task: str):
 
     auto_class = auto_class_task_map.get(task)
     model = auto_class.from_pretrained(repo_id, trust_remote_code = True)  
-
-    config = AutoConfig.from_pretrained(repo_id, trust_remote_code = True)
     
-    return model, config
+    return model
 
-def create_onnx_config(config: AutoConfig, task: str):
+def create_onnx_config(config: PretrainedConfig, task: str) -> OnnxConfig:
+    """
+    Parameters
+    ----------
+    config: AutoConfig
+        AutoConfig config of huggingface model
+    task: str
+        Task of the model
 
+    Returns
+    -------
+    OnnxConfig:
+        onnx config for config's model type and task
+    """
     model_type = config.model_type
-    onnx_config_constructor = onnx_config_constructor_map.get(model_type)
+    if model_type not in onnx_config_constructor_map.keys():
+        onnx_config_constructor = TasksManager.get_exporter_config_constructor('onnx', task = task, model_type = model_type)
+    else:
+        onnx_config_constructor = onnx_config_constructor_map.get(model_type)
     onnx_config = onnx_config_constructor(config, task = task)
 
     return onnx_config    
 
-def export_onnx(repo_id: str , task: str = 'feature-extraction', output: str = ''):
+def export_onnx(repo_id: str , task: str = 'feature-extraction', output_path: str = '', abs_path: str= ''):
     """
     Parameters
     ----------
-    output_path : Path
+    output_path: str
         relative path where model.onnx file will be stored
+    abs_path: str
+        absolute path where model.onnx file will be stored
     """
     if task not in auto_class_task_map.keys():
         # logger error
@@ -45,18 +61,24 @@ def export_onnx(repo_id: str , task: str = 'feature-extraction', output: str = '
 
     #check if path exist
 
-    print(repo_id, task, output)
+    print(repo_id, task, output_path)
 
     # TODO:
     # test this later
     # output might need rework
 
-    # model, config = create_model(repo_id, task)
-    # onnx_config = create_onnx_config(config, task)
-    # onnx_path = Path.cwd() / output / repo_id / 'model.onnx'
-    # onnx_inputs, onnx_outputs = export(model, onnx_config, onnx_path, onnx_config.DEFAULT_ONNX_OPSET)
+    model = create_model(repo_id, task)
+    onnx_config = create_onnx_config(model.config, task)
+    if abs_path:
+        onnx_path = Path(abs_path) / repo_id
+    elif output_path:
+        onnx_path = Path.cwd() / output_path / repo_id 
+    else:
+        onnx_path = Path.cwd() / 'onnx' / repo_id 
+    onnx_path.mkdir(parents=True, exist_ok=True)
+    onnx_inputs, onnx_outputs = export(model, onnx_config, onnx_path / 'model.onnx', onnx_config.DEFAULT_ONNX_OPSET)
 
-    # return onnx_inputs, onnx_outputs
+    return onnx_inputs, onnx_outputs
 
 def parse_arguments():
     parser = argparse.ArgumentParser(prog='export_onnx')
@@ -73,16 +95,22 @@ def parse_arguments():
         help='The model\'s task'
     )
     parser.add_argument(
-        '-o','--output',
+        '-o','--output-path',
         type=str,
         default='',
-        help='output path for model.onnx file'
+        help='relative output path for model.onnx file'
+    ),
+    parser.add_argument(
+        '-a','--abs-path',
+        type=str,
+        default='',
+        help='absolute output path for model.onnx file'
     )
     args = parser.parse_args()
     return args
 
 def main(args):
-    export_onnx(args.repo_id, args.task, args.output)
+    export_onnx(args.repo_id, args.task, args.output_path, args.abs_path)
 
 if __name__ == '__main__':
     args = parse_arguments()
