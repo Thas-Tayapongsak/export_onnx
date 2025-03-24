30cacdc8fdd8dfd2002d03f335fec0466ce10638
Thas-Tayapongsak <hwunhwun@gmail.com>
2025-03-24 16:52:12 +0700

    fix retinanet onnx config (still broken)

--- exporters/model_configs.py
+++ exporters/model_configs.py
@@ -1,4 +1,5 @@
 from optimum.exporters.onnx.model_configs import ViTOnnxConfig
+from optimum.utils import DummyVisionInputGenerator
 
 from typing import OrderedDict, Dict
 
@@ -23,9 +24,43 @@ class MobileNetV3OnnxConfig(ViTOnnxConfig):
         return common_outputs
     
 
+class RetinaNetObjectDetectionInputGenerator(DummyVisionInputGenerator):
+
+    SUPPORTED_INPUT_NAMES = (
+        "pixel_values",
+        "image_sizes"
+    )
+
+    def generate(self, input_name: str, framework: str = "pt", int_dtype: str = "int64", float_dtype: str = "fp32"):
+        if input_name == "image_sizes":
+            return self.random_int_tensor(
+                shape=[self.batch_size, 2],
+                min_value=1,
+                max_value=max(self.height, self.width),
+                framework=framework,
+                dtype=int_dtype,
+            )
+        
+        elif input_name == "pixel_values":
+            return self.random_float_tensor(
+                shape=[self.batch_size, self.num_channels, self.height, self.width],
+                framework=framework,
+                dtype=float_dtype,
+            )
+
 class RetinaNetOnnxConfig(ViTOnnxConfig):
+    DUMMY_INPUT_GENERATOR_CLASSES = (RetinaNetObjectDetectionInputGenerator,)
+
     @property
-    def outputs(self) -> Dict[str, Dict[int, str]]:   
+    def inputs(self) -> Dict[str, Dict[int, str]]:
+        return {
+            "pixel_values": {0: "batch_size", 1: "num_channels", 2: "height", 3: "width"},
+            "image_sizes": {0: "batch_size", 1: "image_dimensions"}
+        }
+
+    @property
+    def outputs(self) -> Dict[str, Dict[int, str]]:
+        
         return OrderedDict(
             {
                 "boxes": {0: "batch_size", 1: "num_predictions", 2: "bbox_coordinates"},
